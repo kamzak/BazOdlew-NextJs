7df08e021e256b6ee0d00a8e86e846403ee77409
kamzak <58105737+kamzak@users.noreply.github.com>
2022-04-16 23:33:38 +0200

    Add files via upload

---
+++ config/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC0_wLEm0kTPqpecoGo0nIxW-rpV-Xt_xU",
+  authDomain: "bazodlew.firebaseapp.com",
+  databaseURL: "https://bazodlew-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bazodlew",
+  storageBucket: "bazodlew.appspot.com",
+  messagingSenderId: "935744604931",
+  appId: "1:935744604931:web:d144f49ff664198d77f736",
+  measurementId: "G-4XHB6GSEVK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();    
+export const database = getDatabase();
